b9dc8790f5a82bdc7d0727831c0452475a6f336a
Leta Malimo <letaemiruleta@gmail.com>
2025-07-13 12:36:20 +0300

    rule fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ export default function ContactPage() {
     <div className="contact-section">
       <h1 className="page-title">Get In Touch</h1>
       <p>
-        I am currently seeking new opportunities and am open to freelance projects. If you have a project in mind, a job opportunity, or just want to connect, please feel free to reach out. I'm always excited to discuss how my skills can help bring your ideas to life.
+        I am currently seeking new opportunities and am open to freelance projects. If you have a project in mind, a job opportunity, or just want to connect, please feel free to reach out. I&apos;m always excited to discuss how my skills can help bring your ideas to life.
       </p>
       <a href="mailto:letaemiruleta@gmail.com" className="button-primary">
         Email Me: letaemiruleta@gmail.com
